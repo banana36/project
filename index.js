d0f20af700429583102173fd8ead1ee633da6df8
Cicala, Giacomo <>
2022-06-29 12:20:38 +0200

    fix

--- index.js
+++ index.js
@@ -3,10 +3,13 @@
  */
 import moment from "moment";
 import localization from "moment/locale/it";
-import { AppRegistry } from "react-native";
+import { AppRegistry, LogBox } from "react-native";
 import "react-native-gesture-handler";
 import App from "./App";
 import { name as appName } from "./app.json";
+
+LogBox.ignoreLogs(["Warning: ..."]); // Ignore log notification by message
+LogBox.ignoreAllLogs(); //Ignore all log notifications
 
 moment.updateLocale("it", localization);
 
